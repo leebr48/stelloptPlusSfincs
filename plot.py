291c6547a10bb6d31c7c134d535fe3e674eea5ba
Brandon Lee <leebr48@gmail.com>
2022-10-24 16:49:35 +0200

    Still broken, but useful comments

--- plot.py
+++ plot.py
@@ -35,9 +35,10 @@ for i,unRegDirectory in enumerate(IOlists['sfincsDir']):
     dataFiles = findFiles('sfincsOutput.h5', directory) # Note that sfincsScan breaks if you use a different output file name, so the default is hard-coded in
 
     # Sort the data
-    allData = {}
-    tempData = {}
+    loadedData = {}
     ErData = {}
+    radData = {} #FIXME new
+    allData = {}
     for file in dataFiles: # Scans through radial directories, and Er directories if present
         
         # Open the output file
@@ -47,15 +48,15 @@ for i,unRegDirectory in enumerate(IOlists['sfincsDir']):
             raise IOError('Unable to open SFINCS output (*.h5) file.')
 
         # Read the desired data from the file
-        tempData['Er'] = f['Er'][()]
-        tempData['FSABjHat'] = f['FSABjHat'][()]
-        tempData['FSABFlow'] = f['FSABFlow'][()]
-        tempData['particleFlux_vm_psiN'] = f['particleFlux_vm_psiN'][()]
-        tempData['particleFlux_vm_rN'] = f['particleFlux_vm_rN'][()]
-        tempData['heatFlux_vm_psiN'] = f['heatFlux_vm_psiN'][()]
-        tempData['heatFlux_vm_rN'] = f['heatFlux_vm_rN'][()]
-        tempData['momentumFlux_vm_psiN'] = f['momentumFlux_vm_psiN'][()]
-        tempData['momentumFlux_vm_rN'] = f['momentumFlux_vm_rN'][()]
+        loadedData['Er'] = f['Er'][()]
+        loadedData['FSABjHat'] = f['FSABjHat'][()]
+        loadedData['FSABFlow'] = f['FSABFlow'][()]
+        loadedData['particleFlux_vm_psiN'] = f['particleFlux_vm_psiN'][()]
+        loadedData['particleFlux_vm_rN'] = f['particleFlux_vm_rN'][()]
+        loadedData['heatFlux_vm_psiN'] = f['heatFlux_vm_psiN'][()]
+        loadedData['heatFlux_vm_rN'] = f['heatFlux_vm_rN'][()]
+        loadedData['momentumFlux_vm_psiN'] = f['momentumFlux_vm_psiN'][()]
+        loadedData['momentumFlux_vm_rN'] = f['momentumFlux_vm_rN'][()]
 
         # Put the data in the proper place
         fileNameToUse = file
@@ -65,26 +66,28 @@ for i,unRegDirectory in enumerate(IOlists['sfincsDir']):
             parentDirOfFile = dirname(fileNameToUse) # Walk up the file directory
 
             if parentDirOfFile == directory:
+                # FIXME maybe now append the r bits?
                 break # We want to work within the given directory, so we can stop going up the tree at this point
             
             nameOfParentDirOfFile = basename(parentDirOfFile)
             dirTypeIndicator = nameOfParentDirOfFile[0].lower()
-            
+
             if dirTypeIndicator == 'p' or dirTypeIndicator == 'r': # You're in a radial directory
                 
                 if ErDirsIncluded:
-                    # FIXME this appears to be busted... only one Er dictionary is ending up in each radial dictionary
-                    allData[nameOfParentDirOfFile] = ErData
+                    # FIXME this appears to be busted... only one Er dictionary (the last one read) is ending up in each radial dictionary
+                    # FIXME you need to add the ErData to another dictionary for the radius, and then append that dictionary to allData!
+                    # FIXME can you just double-name dictionaries? like {{}}?
+                    allData[nameOfParentDirOfFile] = ErData #FIXME we can't do this yet... we have to get the radial dic sorted first, THEN add it to allData
                     ErData = {}
                 else:
-                    allData[nameOfParentDirOfFile] = tempData
+                    allData[nameOfParentDirOfFile] = loadedData
             
             if dirTypeIndicator == 'e' or dirTypeIndicator == 'd': # You're in an Er directory
-                # FIXME this appears to be busted... only one Er dictionary is ending up in each radial dictionary
                 ErDirsIncluded = True
-                ErData[nameOfParentDirOfFile] = tempData
+                ErData[nameOfParentDirOfFile] = loadedData
 
             fileNameToUse = parentDirOfFile
 
-        tempData = {}
+        loadedData = {}
     print(allData)
