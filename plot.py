755ad48afa4345f2e105f621684b23a5e5815f4f
Brandon Lee <leebr48@gmail.com>
2022-10-25 12:44:03 +0200

    File sorting works now

--- plot.py
+++ plot.py
@@ -24,22 +24,34 @@ if all([item == None for item in IOlists['saveLoc']]):
 else:
     saveDefaultTarget = IOlists['saveLoc']
 
+allData = {}
 for i,unRegDirectory in enumerate(IOlists['sfincsDir']):
 
     _, _, _, directory, _ = getFileInfo('/arbitrary/path', unRegDirectory, 'arbitrary')
 
     # Make target directory if it does not exist
-    outDir = makeDir(actualSaveLoc) # Note that this script has file overwrite powers!
-
+    outDir = makeDir(saveDefaultTarget[i]) # Note that this script has file overwrite powers!
+    
     # Retrieve the data
     dataFiles = findFiles('sfincsOutput.h5', directory) # Note that sfincsScan breaks if you use a different output file name, so the default is hard-coded in
 
-    # Sort the data
+    # Sort out the SFINCS subdirectories
+    subdirs = [item.replace(directory+'/','') for item in dataFiles]
+
+    splitSubdirs = [item.split('/') for item in subdirs]
+
+    dataDepth = len(splitSubdirs[0])
+
+    if not all([len(item) == dataDepth for item in splitSubdirs]):
+        raise IOError('The structure of the SFINCS directory {} does not seem to be normal.'.format(directory))
+
+    # Cycle through each file, read its data, and put that data in the proper place
+    #radData = {}
+    radDirName = None
     loadedData = {}
-    ErData = {}
-    radData = {} #FIXME new
-    allData = {}
-    for file in dataFiles: # Scans through radial directories, and Er directories if present
+    for j,file in enumerate(dataFiles): # Scans through radial directories, and Er directories if present
+
+        subdictNames = splitSubdirs[j]
         
         # Open the output file
         try:
@@ -58,36 +70,25 @@ for i,unRegDirectory in enumerate(IOlists['sfincsDir']):
         loadedData['momentumFlux_vm_psiN'] = f['momentumFlux_vm_psiN'][()]
         loadedData['momentumFlux_vm_rN'] = f['momentumFlux_vm_rN'][()]
 
-        # Put the data in the proper place
-        fileNameToUse = file
-        ErDirsIncluded = False
+        # Put the data in the appropriate place
+        if dataDepth == 2: # Only radial directories are present
+            allData[subdictNames[0]] = loadedData
+        
+        elif dataDepth == 3: # Radial and Er directories are present
 
-        while True:
-            parentDirOfFile = dirname(fileNameToUse) # Walk up the file directory
-
-            if parentDirOfFile == directory:
-                # FIXME maybe now append the r bits?
-                break # We want to work within the given directory, so we can stop going up the tree at this point
+            if radDirName == subdictNames[0]: # You are in the same radial directory as in the last iteration over dataFiles
+                radData[subdictNames[1]] = loadedData
             
-            nameOfParentDirOfFile = basename(parentDirOfFile)
-            dirTypeIndicator = nameOfParentDirOfFile[0].lower()
+            else: # You are in a different radial directory from the last iteration over dataFiles
+                if j != 0: # We shouldn't try to append data on the first loop iteration
+                    allData[radDirName] = radData
+                radDirName = subdictNames[0]
+                radData = {}
+                radData[subdictNames[1]] = loadedData
+        
+        else:
+            raise IOError('The structure of the SFINCS directory {} does not seem to be normal.'.format(directory))
 
-            if dirTypeIndicator == 'p' or dirTypeIndicator == 'r': # You're in a radial directory
-                
-                if ErDirsIncluded:
-                    # FIXME this appears to be busted... only one Er dictionary (the last one read) is ending up in each radial dictionary
-                    # FIXME you need to add the ErData to another dictionary for the radius, and then append that dictionary to allData!
-                    # FIXME can you just double-name dictionaries? like {{}}?
-                    allData[nameOfParentDirOfFile] = ErData #FIXME we can't do this yet... we have to get the radial dic sorted first, THEN add it to allData
-                    ErData = {}
-                else:
-                    allData[nameOfParentDirOfFile] = loadedData
-            
-            if dirTypeIndicator == 'e' or dirTypeIndicator == 'd': # You're in an Er directory
-                ErDirsIncluded = True
-                ErData[nameOfParentDirOfFile] = loadedData
+        loadedData = {} # This should be clean for each new file
 
-            fileNameToUse = parentDirOfFile
-
-        loadedData = {}
-    print(allData)
+    allData = {} # This should be clean for each new directory
